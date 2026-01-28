7e13e9c6b0e07828a69db70a9b42da48fd8f0190
Leemainsw <yebom6476@gmail.com>
2026-01-28 14:35:30 +0900

    solve: 해시 > 문제 19 문자열 해싱을 이용한 검색 함수 만들기

---
+++ solutions/yesul/0128/[문제19]문자열해싱을이용한검색함수만들.js
@@ -0,0 +1,8 @@
+// function solution(stringList, queryList) {
+//     return queryList.map((query) => stringList.includes(query));
+// }
+
+function solution(stringList, queryList) {
+}
+
+console.log(solution(["apple", "banana", "charry"], ["banana", "kiwi", "melon", "apple"])); // [true, false, false, true]
