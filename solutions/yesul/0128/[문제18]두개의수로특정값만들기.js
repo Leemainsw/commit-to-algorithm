4caf141a41c65d6de1ae24e22410d96db3515b07
Leemainsw <yebom6476@gmail.com>
2026-01-28 14:35:18 +0900

    solve: 해시 > 문제 18 두개이 수로 특정값 만들기

---
+++ solutions/yesul/0128/[문제18]두개의수로특정값만들기.js
@@ -0,0 +1,14 @@
+function solution(arr, target) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[i] + arr[j] === target) {
+                return true; ll
+            }
+        }
+    }
+
+    return false;
+}
+
+console.log(solution([1, 2, 3, 4, 8], 6)); // true
+console.log(solution([2, 3, 5, 9], 10)); // false
